ee5cd9d0d59186ec096375f56894430dade616f4
Sanyal <nilabjo.sanyal@accenture.com>
2019-02-19 17:32:54 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-var arc = document.getElementById('arc');
-var ctx = arc.getContext('2d');
+var colorBtns = document.querySelectorAll('button[data-identifier^="sample"]');
+var spotlight = document.querySelector('.spotlight-img > img');
+var selected = document.querySelector('.active-ring');
+var colorName = document.querySelector('.color-name');
+colorName.style.color = selected.dataset.identifier.split('-')[1];
 
-function drawPieSlice(
-  ctx,
-  centerX,
-  centerY,
-  radius,
-  startAngle,
-  endAngle,
-  color
-) {
-  ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.moveTo(centerX, centerY);
-  ctx.arc(centerX, centerY, radius, startAngle, endAngle);
-  ctx.closePath();
-  ctx.fill();
+function clearHighlight() {
+  for (var btn of colorBtns) {
+    if (btn.classList.contains('active-ring')) {
+      btn.classList.remove('active-ring');
+    }
+  }  
 }
 
-drawPieSlice(ctx, 150, 0, 150, 0, Math.PI / 2 + Math.PI / 4, '#ff0000');
+for (var i = 0; i < colorBtns.length; i++) {
+  colorBtns[i].addEventListener('click', function (e) {
+    clearHighlight();
+    e.target.classList.add('active-ring');
+    var color = e.target.dataset.identifier.split('-')[1];
+    spotlight.src = 'images/' + color + '.jpg';
+    colorName.innerHTML = color;
+    colorName.style.color = color;
+  })
+}
